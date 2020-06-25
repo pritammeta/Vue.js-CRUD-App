a785cf6d1f1e63c5a35d865ffd8ed78eb3c6d8d0
pritam <pritam@metapercept.com>
2020-06-25 17:45:50 +0530

    Firebase and vuejs successfully integrated.
    Signup and login views is working properly with firebase auth.

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+ Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signin.vue')
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+  },
+   
+]
+
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Signin.vue
@@ -0,0 +1,88 @@
+<template>
+   
+   <div class="login">
+       <h1>Vue.js Firebase App</h1>
+     <img alt="Vue logo" src="../assets/logo.png"><br> 
+     <h2>Welcome back !</h2>
+      <p> {{ message }}</p>
+      <input type="email" v-model="email"  placeholder="Email.."><br>
+     <input type="password" v-model="password"  placeholder="Password."><br>
+     <button v-on:click="login">Login</button>
+      <p>Don't, have account? <router-link to=/signup> Sign up</router-link></p>
+
+ </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+export default {
+    name: 'login',
+    data(){
+        return{
+          message:'',
+          email:'',
+          password:''
+        };
+    },
+    methods: {
+        login: function(){
+                  this.message='Just wait...';
+
+              firebase
+              .auth()
+              .signInWithEmailAndPassword(this.email,this.password)
+              .then(
+                ()=> {
+
+                  this.message='Logged in Successfully.';
+                 // alert('Welcome Again!');
+                           //     this.$router.replace('home');
+
+                },
+                (err) => {
+                    alert('Opps,'+err.message);
+                                      this.message='Please, try again...';
+
+                },
+              )
+
+        }
+    }
+};
+</script>>
+
+
+ <style>
+body {
+  font-family: Arial;
+}
+
+input{
+   padding: 12px 20px;
+  margin: 8px 0;
+   border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+ 
+button {
+ background-color: #4DAB68;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;}
+ 
+
+div.login {
+   border-radius: 5px;
+  background-color: whitesmoke;
+  padding: 10px;
+}
+p{
+    font-size: 12px;
+    cursor: pointer;
+}
+</style>
